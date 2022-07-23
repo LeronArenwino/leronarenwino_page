77aa23d65fcc0d1444ab5b669a0e60f4ebeb6edb
Francsico Dueñas <frsduenasca@unal.edu.co>
2022-07-23 17:40:45 -0500

    :zap: New design to HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,83 @@
 <script setup></script>
 
 <template>
-  <main class="container">
-    <p>Home</p>
+  <main>
+    <section class="bg-white dark:bg-gray-900">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+        <div class="max-w-screen-md mb-8 lg:mb-16">
+          <h2
+            class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
+          >
+            About me
+          </h2>
+          <p class="text-gray-500 sm:text-xl dark:text-gray-400">
+            Hello!, I'm a student of systems and computer engineer with
+            experience in Frontend developer with 1 year of experience in
+            software development for single page app and other information
+            technologies.
+          </p>
+        </div>
+        <div
+          class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0"
+        >
+          <div>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">
+              Web development
+            </h3>
+            <p class="text-gray-500 dark:text-gray-400">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+          <div>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">Design</h3>
+            <p class="text-gray-500 dark:text-gray-400">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+          <div>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">Automation</h3>
+            <p class="text-gray-500 dark:text-gray-400">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+          <div>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">Languages</h3>
+            <p class="text-gray-500 dark:text-gray-400">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+          <div>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">Work</h3>
+            <p class="text-gray-500 dark:text-gray-400">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+          <div>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">Others</h3>
+            <p class="text-gray-500 dark:text-gray-400">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
