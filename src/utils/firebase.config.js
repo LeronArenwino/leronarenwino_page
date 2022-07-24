d06cdb0e246ad5f34b8594b14c7bb5423a93d5da
Francsico Dueñas <frsduenasca@unal.edu.co>
2022-07-24 00:43:20 -0500

    :zap: Added firebase configuration

---
+++ src/utils/firebase.config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWxS2s53Qq0aEQsseZMXrxafyHcKWrpIc",
+  authDomain: "leron-arenwino.firebaseapp.com",
+  projectId: "leron-arenwino",
+  storageBucket: "leron-arenwino.appspot.com",
+  messagingSenderId: "485253705751",
+  appId: "1:485253705751:web:94ee17b38d8d71fda4ac52",
+};
+
+// Exports
+export default {
+  firebaseConfig,
+};
