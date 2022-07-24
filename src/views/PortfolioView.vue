320b5a72e33e03bc694055d578cdba3f81fd1fc5
Francsico Dueñas <frsduenasca@unal.edu.co>
2022-07-24 01:06:25 -0500

    :zap: New template for all projects

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -12,83 +12,48 @@
         </p>
       </div>
       <div class="grid gap-8 lg:grid-cols-2">
-        <article
-          class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
-        >
-          <div class="flex justify-between items-center mb-5 text-gray-500">
-            <span
-              class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800"
-            >
-              Development in Java and MySQL
-            </span>
-            <span class="text-sm">14 days ago</span>
-          </div>
-          <h2
-            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        <template v-for="project of projects" :key="project.id">
+          <article
+            class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
           >
-            <a href="#">Restaurant application</a>
-          </h2>
-          <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
-            This is a desktop application developed as a final project of
-            Software Engineering I, which seeks to offer the restaurant service
-            of a cruise ship, implementing CRUD operations with a MYSQL
-            database.
-          </p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center space-x-4">
-              <img
-                class="w-7 h-7 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
-                alt="Bonnie Green avatar"
-              />
-              <span class="font-medium dark:text-white"> GitHub </span>
+            <div class="flex justify-between items-center mb-5 text-gray-500">
+              <span
+                class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800"
+              >
+                {{ project.development }}
+              </span>
+              <span class="text-sm">{{
+                project.date_published.toDate().toDateString()
+              }}</span>
             </div>
-            <a
-              href="#"
-              class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
+            <h2
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
             >
-              Read more
-            </a>
-          </div>
-        </article>
-        <article
-          class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+              <a href="">{{ project.name }}</a>
+            </h2>
+            <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
+              {{ project.description }}
+            </p>
+            <div class="flex justify-between items-center">
+              <div class="flex items-center space-x-4">
+                <img
+                  class="w-7 h-7 rounded-full"
+                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+                  alt="Bonnie Green avatar"
+                />
+                <span class="font-medium dark:text-white"> GitHub </span>
+              </div>
+              <template v-if="project.link_website != ''">
+                <a
+                  :href="project.link_website"
+                  class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
+                >
+                  Look the project!
+                </a></template
+              >
+            </div>
+          </article></template
         >
-          <div class="flex justify-between items-center mb-5 text-gray-500">
-            <span
-              class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800"
-            >
-              Development in Vue, Bootstrap, HTML, JS, Spring Boot and Posgrest
-            </span>
-            <span class="text-sm">14 days ago</span>
-          </div>
-          <h2
-            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-          >
-            <a href="#">Wheels US</a>
-          </h2>
-          <p class="mb-5 font-light text-gray-500 dark:text-gray-400">
-            This is a web application developed as a final project of Software
-            Engineering II, which seeks to offer the service of conect
-            universitary people to travel together to University or home.
-          </p>
-          <div class="flex justify-between items-center">
-            <div class="flex items-center space-x-4">
-              <img
-                class="w-7 h-7 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
-                alt="Bonnie Green avatar"
-              />
-              <span class="font-medium dark:text-white"> GitHub </span>
-            </div>
-            <a
-              href="#"
-              class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
-            >
-              Read more
-            </a>
-          </div>
-        </article>
       </div>
     </div>
   </section>
