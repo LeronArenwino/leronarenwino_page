aee0a4e5418af301ad1244aff59bc0337b86863b
Francsico Dueñas <frsduenasca@unal.edu.co>
2022-07-30 10:00:21 -0500

    :zap: Added store to general project

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 import { RouterView } from "vue-router";
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import FooterComponent from "./components/FooterComponent.vue";
+import { useProjectStore } from "./stores/index";
+
+const store = useProjectStore();
+
+store.getProjects();
 </script>
 
 <template>
