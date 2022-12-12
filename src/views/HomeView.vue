fcb4b6839d1f02f74ab1591f6e42e4dfda7f7656
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-12-11 21:40:54 -0500

    :zap: New design to knowledge section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,7 +43,7 @@ const store = useProjectStore();
         <div class="grid gap-8 lg:grid-cols-2">
           <template v-for="project of store.projects" :key="project.id">
             <article
-              class="p-6 bg-gradient-to-r to-white bg-emerald-200 from-emerald-300 rounded-lg border border-gray-200 shadow-lg dark:bg-gray-800 dark:border-gray-700"
+              class="p-6 bg-gradient-to-r to-white bg-emerald-200 from-emerald-300 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700"
             >
               <div class="flex justify-between items-center mb-5 text-gray-500">
                 <span
@@ -112,117 +112,38 @@ const store = useProjectStore();
         <div
           class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0"
         >
-          <!-- Pricing Card -->
           <div
-            class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white"
+            class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-gradient-to-r to-white bg-emerald-200 from-emerald-300 rounded-lg shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white"
           >
             <h3 class="mb-4 text-2xl font-semibold">Programming languages</h3>
-            <p class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-              Best option for personal use & for your next project.
+            <p class="font-light text-black sm:text-lg dark:text-gray-400">
+              My mastery of the programming language from 0% to 100%.
             </p>
-            <div class="flex justify-center items-baseline my-8">
-              <span class="mr-2 text-5xl font-extrabold">$29</span>
-              <span class="text-gray-500 dark:text-gray-400">/month</span>
-            </div>
-            <!-- List -->
             <ul role="list" class="mb-8 space-y-4 text-left">
-              <li class="flex items-center space-x-3">
-                <!-- Icon -->
-                <svg
-                  class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+              <li
+                class="flex flex-col justify-center items-center space-y-2 my-2"
+              >
+                <img class="h-16" src="../assets/java-dark.svg" alt="" />
+                <div
+                  class="bg-emerald-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                  style="width: 45%"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span>Individual configuration</span>
+                  45%
+                </div>
               </li>
-              <li class="flex items-center space-x-3">
-                <!-- Icon -->
-                <svg
-                  class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+              <li
+                class="flex flex-col justify-center items-center space-y-2 my-2"
+              >
+                <img class="h-16" src="../assets/python-dark.svg" alt="" />
+                <div
+                  class="bg-emerald-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                  style="width: 45%"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span>No setup, or hidden fees</span>
-              </li>
-              <li class="flex items-center space-x-3">
-                <!-- Icon -->
-                <svg
-                  class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span
-                  >Team size:
-                  <span class="font-semibold">1 developer</span></span
-                >
-              </li>
-              <li class="flex items-center space-x-3">
-                <!-- Icon -->
-                <svg
-                  class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span
-                  >Premium support:
-                  <span class="font-semibold">6 months</span></span
-                >
-              </li>
-              <li class="flex items-center space-x-3">
-                <!-- Icon -->
-                <svg
-                  class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span
-                  >Free updates:
-                  <span class="font-semibold">6 months</span></span
-                >
+                  45%
+                </div>
               </li>
             </ul>
-            <a
-              href="#"
-              class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white dark:focus:ring-primary-900"
-              >Get started</a
-            >
           </div>
-          <!-- /Pricing Card -->
         </div>
       </div>
     </section>
