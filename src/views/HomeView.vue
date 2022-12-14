b7be1ab400e9bf176d824b806cdbc6a60c996548
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-12-14 09:13:28 -0500

    :zap: New design to about section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -72,7 +72,7 @@ const store = useDataStore();
                   <button
                     type="button"
                     @click="redirectTo(project.link_github)"
-                    class="flex justify-center items-center text-black transition ease-in-out delay-50 bg-white hover:-translate-y-1 hover:scale-110 focus:outline-none hover:bg-emerald-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                    class="flex justify-center items-center text-black transition ease-in-out delay-50 bg-white hover:-translate-y-1 hover:scale-110 focus:outline-none hover:bg-emerald-100 focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600"
                   >
                     <img
                       class="w-6 h-6 rounded-full mr-2"
@@ -152,18 +152,18 @@ const store = useDataStore();
       </div>
     </section>
     <section id="about" class="bg-transparent dark:bg-gray-900 my-16">
-      <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-        <div class="max-w-screen-md mb-8 lg:mb-16">
-          <h2
-            class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
+      <div class="py-16 px-4 mx-auto max-w-screen-xl lg:px-6">
+        <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
+          <h1
+            class="my-4 text-4xl leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
           >
-            About me
-          </h2>
-          <p class="text-gray-500 sm:text-xl dark:text-gray-400">
-            Hello!, I'm a student of systems and computer engineer with
-            experience in Frontend developer with 1 year of experience in
-            software development for single page app and other information
-            technologies.
+            <span
+              class="text-transparent font-medium bg-clip-text bg-gradient-to-r to-teal-700 from-emerald-700"
+              >About me</span
+            >
+          </h1>
+          <p class="mb-5 font-light text-black sm:text-xl dark:text-gray-400">
+            Information about me:
           </p>
         </div>
         <div
