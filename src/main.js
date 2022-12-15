64679b9b7356fa90c830c8bbe8d49544387f9990
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-12-15 14:07:15 -0500

    :zap: Import and using fontawesome

--- src/main.js
+++ src/main.js
@@ -5,12 +5,29 @@ import App from "./App.vue";
 import router from "./router";
 import "./index.css";
 import "flowbite";
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+  faLinkedin,
+  faGithub,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUserSecret, faLinkedin, faGithub, faTwitter);
 
 const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
 
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
 
 var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
