3ac982d2b9ee3391903954724108c5b8c59bf76a
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-01-25 17:04:19 -0500

    :zap: New design to YardSaleSignUpVview

--- src/views/yardsale/YardSaleSignUpView.vue
+++ src/views/yardsale/YardSaleSignUpView.vue
@@ -4,32 +4,36 @@
       <div class="form-container">
         <h1 class="title dark:text-white">My account</h1>
         <form action="/" class="form">
-          <label for="name" class="label dark:text-white">Name</label>
-          <input
-            type="text"
-            id="name"
-            placeholder="Leron"
-            class="input input-name"
-          />
-          <label for="email" class="label dark:text-white">Email</label>
-          <input
-            type="text"
-            id="email"
-            placeholder="leronarenwino@example.com"
-            class="input input-email"
-          />
-          <label for="password" class="label dark:text-white">Password</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="*********"
-            class="input input-password"
-          />
-          <input
-            type="submit"
-            value="Create"
-            class="text-white transition ease-in-out delay-50 bg-emerald-400 hover:-translate-y-1 hover:scale-110 hover:bg-emerald-500 duration-300 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 mr-2 mb-2 button-primary login-button"
-          />
+          <div>
+            <label for="name" class="label dark:text-white">Name</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Leron"
+              class="input input-name"
+            />
+            <label for="email" class="label dark:text-white">Email</label>
+            <input
+              type="text"
+              id="email"
+              placeholder="leronarenwino@example.com"
+              class="input input-email"
+            />
+            <label for="password" class="label dark:text-white">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="*********"
+              class="input input-password"
+            />
+          </div>
+          <div>
+            <input
+              type="submit"
+              value="Create"
+              class="text-white transition ease-in-out delay-50 bg-emerald-400 hover:-translate-y-1 hover:scale-110 hover:bg-emerald-500 duration-300 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 mr-2 mb-2 button-primary login-button"
+            />
+          </div>
         </form>
       </div>
     </section>
@@ -69,6 +73,10 @@
   display: flex;
   flex-direction: column;
 }
+.form div {
+  display: flex;
+  flex-direction: column;
+}
 .label {
   font-size: var(--sm);
   font-weight: bold;
@@ -102,5 +110,12 @@
   cursor: pointer;
 }
 @media (max-width: 640px) {
+  .form-container {
+    height: 100%;
+  }
+  .form {
+    height: 100%;
+    justify-content: space-between;
+  }
 }
 </style>
