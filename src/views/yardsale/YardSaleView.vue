99693aaa8171d1347c71ee3d7b6298d2416bcd9c
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-01 18:38:36 -0500

    :zap: New design using YardSaleMenuComponent

--- src/views/yardsale/YardSaleView.vue
+++ src/views/yardsale/YardSaleView.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { RouterView } from "vue-router";
+import YardSaleMenuComponent from "@/components/YardSaleMenuComponent.vue";
+</script>
+
 <template>
-  <router-view></router-view>
+  <YardSaleMenuComponent></YardSaleMenuComponent>
+  <RouterView />
 </template>
