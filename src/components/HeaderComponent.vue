cae123237e978843faecb1f5b07ed0602cd255b0
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-01 07:42:01 -0500

    :zap: New design to HeaderComponent

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -7,13 +7,8 @@
         class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl"
       >
         <router-link to="/">
-          <a class="flex items-center">
-            <img
-              src="../assets/logo.png"
-              class="mr-3 h-6 sm:h-9"
-              alt="Leron Logo"
-            /> </a
-        ></router-link>
+          <font-awesome-icon class="dark:text-white" icon="fa-solid fa-house"
+        /></router-link>
         <div class="flex items-center lg:order-2">
           <button
             data-collapse-toggle="navbar-default"
@@ -48,6 +43,7 @@
             </svg>
           </button>
         </div>
+
         <div
           class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
           id="navbar-default"
@@ -55,30 +51,44 @@
           <ul
             class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0"
           >
-            <li>
-              <a
-                class="block py-2 pr-4 pl-3 text-white bg-emerald-700 rounded md:bg-transparent md:text-emerald-700 md:p-0 dark:text-white"
-                aria-current="page"
-                href="#app"
-                >Home</a
-              >
-            </li>
-            <li>
-              <a
-                class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-emerald-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                aria-current="page"
-                href="#portfolio"
-                >Portfolio</a
-              >
-            </li>
-            <li>
-              <a
-                class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-emerald-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                aria-current="page"
-                href="#knowledge"
-                >Knowledge</a
-              >
-            </li>
+            <template v-if="$route.path == '/'">
+              <li>
+                <a
+                  class="block py-2 pr-4 pl-3 text-white bg-emerald-700 rounded md:bg-transparent md:text-emerald-700 md:p-0 dark:text-white"
+                  aria-current="page"
+                  href="#app"
+                  >Home</a
+                >
+              </li>
+              <li>
+                <a
+                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-emerald-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  aria-current="page"
+                  href="#portfolio"
+                  >Portfolio</a
+                >
+              </li>
+              <li>
+                <a
+                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-emerald-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  aria-current="page"
+                  href="#knowledge"
+                  >Knowledge</a
+                >
+              </li>
+            </template>
+            <template v-else>
+              <li>
+                <router-link to="/">
+                  <a
+                    class="block py-2 pr-4 pl-3 text-white bg-emerald-700 rounded md:bg-transparent md:text-emerald-700 md:p-0 dark:text-white"
+                    aria-current="page"
+                    href=""
+                    >Home</a
+                  >
+                </router-link>
+              </li>
+            </template>
           </ul>
         </div>
       </div>
