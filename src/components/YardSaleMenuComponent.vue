781fa261f52cbd530f7287c8e1284d96c1781a67
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-04 19:35:33 -0500

    :zap: New design to YardSaleMenuComponent

--- src/components/YardSaleMenuComponent.vue
+++ src/components/YardSaleMenuComponent.vue
@@ -6,30 +6,45 @@
       <img src="../assets/icon_menu.svg" alt="menu" class="menu" />
       <div class="navbar-left">
         <img src="../assets/logo_yard_sale.svg" alt="logo" class="logo" />
-        <ul>
-          <li>
+        <ul class="text-gray-500 dark:text-green-400">
+          <li
+            class="hover:text-gray-700 dark:hover:text-green-200 hover:border hover:border-gray-700 dark:hover:border-green-200 hover:rounded"
+          >
             <a href="/">All</a>
           </li>
-          <li>
+          <li
+            class="hover:text-gray-700 dark:hover:text-green-200 hover:border hover:border-gray-700 dark:hover:border-green-200 hover:rounded"
+          >
             <a href="/">Clothes</a>
           </li>
-          <li>
+          <li
+            class="hover:text-gray-700 dark:hover:text-green-200 hover:border hover:border-gray-700 dark:hover:border-green-200 hover:rounded"
+          >
             <a href="/">Electronics</a>
           </li>
-          <li>
+          <li
+            class="hover:text-gray-700 dark:hover:text-green-200 hover:border hover:border-gray-700 dark:hover:border-green-200 hover:rounded"
+          >
             <a href="/">Toys</a>
           </li>
-          <li>
+          <li
+            class="hover:text-gray-700 dark:hover:text-green-200 hover:border hover:border-gray-700 dark:hover:border-green-200 hover:rounded"
+          >
             <a href="/">Others</a>
           </li>
         </ul>
       </div>
       <div class="navbar-right">
         <ul>
-          <li>leronarenwino@example.com</li>
+          <li class="text-gray-500 dark:text-green-400 navbar-email">
+            leronarenwino@example.com
+          </li>
           <li class="navbar-shopping-cart">
-            <font-awesome-icon icon="fa-solid fa-cart-shopping" />
-            <div></div>
+            <font-awesome-icon
+              class="dark:text-white"
+              icon="fa-solid fa-cart-shopping"
+            />
+            <div class="rounded-full bg-green-400 text-sm font-bold">2</div>
           </li>
         </ul>
       </div>
@@ -86,5 +101,58 @@
 .desktop-menu ul li a {
   display: inline-block;
   margin-bottom: 16px;
+}
+.menu {
+  display: none;
+}
+.logo {
+  width: 100px;
+}
+.navbar-left {
+  display: flex;
+}
+
+.navbar-left ul,
+.navbar-right ul {
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+}
+.navbar-left ul {
+  margin-left: 12px;
+}
+.navbar-left ul li a,
+.navbar-right ul li a {
+  padding: 8px;
+}
+nav {
+  display: flex;
+  justify-content: space-between;
+}
+.navbar-email {
+  margin-right: 12px;
+}
+.navbar-shopping-cart div {
+  width: 16px;
+  height: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 4px;
+  right: 12px;
+}
+
+@media (max-width: 640px) {
+  .menu {
+    display: block;
+  }
+  .navbar-left ul {
+    display: none;
+  }
+  .navbar-email {
+    display: none;
+  }
 }
 </style>
