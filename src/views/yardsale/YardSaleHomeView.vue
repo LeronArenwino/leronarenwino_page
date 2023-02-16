5aa604c31caf0802cb9fd5d34346d651a3ef76cf
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-15 21:24:50 -0500

    :zap: [UPDATE] Fetch to get products from API

--- src/views/yardsale/YardSaleHomeView.vue
+++ src/views/yardsale/YardSaleHomeView.vue
@@ -1,279 +1,91 @@
 <template>
   <main class="container mx-auto main-container">
     <div class="cards-container">
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
+      <div v-for="item in products" :key="item.id">
+        <div class="product-card">
+          <img :src="item.images[0]" alt="Image product" />
+          <div class="product-info">
+            <div>
+              <p class="dark:text-white">{{ item.title }}</p>
+              <p class="text-gray-400 dark:text-gray-300">${{ item.price }}</p>
+            </div>
+            <figure>
+              <font-awesome-icon
+                class="w-8 h-8 dark:text-white"
+                icon="fa-solid fa-cart-plus"
+              />
+            </figure>
           </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
-        </div>
-      </div>
-      <div class="product-card">
-        <img
-          src="https://images.pexels.com/photos/34006/pexels-photo.jpg"
-          alt=""
-        />
-        <div class="product-info">
-          <div>
-            <p class="dark:text-white">$120,00</p>
-            <p class="text-gray-400 dark:text-gray-300">Motorbike</p>
-          </div>
-          <figure>
-            <font-awesome-icon
-              class="w-8 h-8 dark:text-white"
-              icon="fa-solid fa-cart-plus"
-            />
-          </figure>
         </div>
       </div>
     </div>
   </main>
 </template>
+
+<script>
+// API URL
+const API = "https://api.escuelajs.co/api/v1";
+
+// Elements
+const desktopMenu = document.getElementById("desktop-menu");
+const mobileMenu = document.getElementById("mobile-menu");
+const productDetail = document.getElementById("product-detail");
+
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    toggleDesktopMenu() {
+      const isProductDetailClosed =
+        productDetail.classList.contains("inactive");
+
+      if (!isProductDetailClosed) {
+        productDetail.classList.add("inactive");
+      }
+
+      desktopMenu.classList.toggle("inactive");
+    },
+    toggleMobileMenu() {
+      const isProductDetailClosed =
+        productDetail.classList.contains("inactive");
+
+      if (!isProductDetailClosed) {
+        productDetail.classList.add("inactive");
+      }
+
+      mobileMenu.classList.toggle("inactive");
+    },
+    toggleProductDetail() {
+      const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+      const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+      if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+      }
+      if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add("inactive");
+      }
+
+      productDetail.classList.toggle("inactive");
+    },
+    async fetchData(urlApi) {
+      const response = await fetch(urlApi);
+      const data = await response.json();
+      return data;
+    },
+    async getProducts(urlApi) {
+      this.products = [];
+      this.products = await this.fetchData(`${urlApi}/products?`);
+    },
+  },
+  mounted() {
+    this.getProducts(API);
+  },
+};
+</script>
 
 <style scoped>
 :root {
