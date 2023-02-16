c68205f914299af080687e6cfb678f60989ab970
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-15 23:01:46 -0500

    :zap:[UPDATE] Using getProducts method

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@ import HeaderComponent from "@/components/HeaderComponent.vue";
 import FooterComponent from "./components/FooterComponent.vue";
 import { useDataStore } from "@/stores/index.js";
 
+// API URL
+const API = "https://api.escuelajs.co/api/v1";
+
 const store = useDataStore();
 
 store.getProjects();
 store.getKnowledges();
+store.getProducts(API);
 </script>
 
 <template>
