f982e2aa7ab13dae2c5b8a54c3126444a679d300
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-04-07 17:30:03 -0500

    :zap: [UPDATE] New design to HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,38 +5,44 @@ const store = useDataStore();
 </script>
 
 <template>
-  <main class="container mx-auto bg-transparent">
+  <main class="bg-transparent mx-0">
     <section
-      class="grid gap-4 py-8 px-4 mx-auto max-w-screen-xl lg:px-12 h-screen place-content-center text-center"
+      class="grid gap-4 py-8 px-4 lg:px-36 h-screen place-content-center text-center bg-emerald-500"
     >
-      <h1
-        class="my-4 text-4xl text-black md:text-6xl lg:text-8xl dark:text-white font-medium"
+      <div
+        class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow-2xl sm:p-8 dark:bg-gray-800 dark:border-gray-700"
       >
-        <span
-          class="text-transparent bg-clip-text bg-gradient-to-r to-teal-700 from-emerald-700"
-          >Hello, welcome!</span
+        <h1
+          class="my-4 text-4xl text-black md:text-6xl lg:text-8xl dark:text-white font-medium"
         >
-      </h1>
-
-      <p
-        class="mb-8 text-lg font-thin text-black lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
-      >
-        I'm a systems engineer from National University of Colombia interested
-        in development of applications. I have worked on different projects,
-        developed mainly with Java, Python, Javascript and C.
-      </p>
+          <span class="text-transparent bg-clip-text bg-black dark:bg-white"
+            >Hello, welcome!</span
+          >
+        </h1>
+        <p
+          class="mb-8 text-lg font-thin text-black lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
+        >
+          I'm a Systems Engineer from National University of Colombia interested
+          in development of applications. I have worked on different projects,
+          developed mainly with Java, Python, Javascript and C.
+        </p>
+      </div>
     </section>
-    <section
-      id="portfolio"
-      class="container bg-transparent dark:bg-transparent my-16"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path
+        class="fill-emerald-500"
+        fill-opacity="1"
+        d="M0,192L30,197.3C60,203,120,213,180,181.3C240,149,300,75,360,69.3C420,64,480,128,540,181.3C600,235,660,277,720,272C780,267,840,213,900,176C960,139,1020,117,1080,122.7C1140,128,1200,160,1260,186.7C1320,213,1380,235,1410,245.3L1440,256L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"
+      ></path>
+    </svg>
+    <section id="portfolio" class="bg-transparent dark:bg-transparent py-16">
       <div class="py-16 px-4 mx-auto max-w-screen-xl lg:px-6">
         <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
           <h1
             class="my-4 text-4xl leading-none text-black md:text-5xl lg:text-6xl dark:text-white"
           >
             <span
-              class="text-transparent font-medium bg-clip-text bg-gradient-to-r to-teal-700 from-emerald-700"
+              class="font-mono text-transparent font-medium bg-clip-text bg-black dark:bg-white"
               >Portfolio</span
             >
           </h1>
@@ -44,38 +50,44 @@ const store = useDataStore();
             Projects I have worked on!
           </p>
         </div>
-        <div class="grid gap-8 lg:grid-cols-2">
-          <template v-for="project of store.projects" :key="project.id">
-            <article
-              class="p-6 bg-gradient-to-r to-white via-emerald-200 from-emerald-300 rounded-lg shadow-lg dark:bg-gradient-to-r dark:to-gray-800 dark:from-emerald-800 transform transition-all hover:-translate-y-2 hover:shadow-2xl"
-            >
-              <div
-                class="flex flex-col justify-center space-y-2 sm:flex-row sm:space-y-0 sm:justify-between items-center mb-5 text-gray-500"
-              >
-                <span
-                  class="bg-emerald-100 text-center text-emerald-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-emerald-200 dark:text-emerald-800"
+        <div
+          class="w-full p-4 bg-emerald-500 border border-gray-200 rounded-lg shadow-2xl sm:p-8 dark:bg-gray-800 dark:border-gray-700"
+        >
+          <ol class="relative border-l border-indigo-200 dark:border-gray-700">
+            <template v-for="project of store.projects" :key="project.id">
+              <li class="mb-10 ml-4">
+                <div
+                  class="absolute w-3 h-3 bg-emerald-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+                ></div>
+                <time
+                  class="mb-1 text-sm font-normal leading-none text-gray-100 dark:text-gray-500"
+                  >Released on
+                  {{
+                    project.date_published
+                      .toDate()
+                      .toLocaleDateString("en-US", {
+                        weekday: "long",
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })
+                  }}</time
                 >
-                  {{ project.development }}
-                </span>
-                <span
-                  class="text-sm text-transparent bg-clip-text bg-emerald-700 dark:bg-emerald-200"
-                  >{{ project.date_published.toDate().toDateString() }}</span
+                <h3
+                  class="text-2xl font-semibold text-gray-900 dark:text-white"
                 >
-              </div>
-              <h2
-                class="mb-2 text-2xl font-normal text-center lg:text-left text-gray-900 dark:text-white"
-              >
-                <h5>{{ project.name }}</h5>
-              </h2>
-              <p
-                class="mb-5 font-light text-center lg:text-left text-black dark:text-gray-300"
-              >
-                {{ project.description }}
-              </p>
-              <div
-                class="flex flex-col space-y-2 justify-center sm:flex-row sm:space-y-0 sm:justify-between items-center"
-              >
-                <div class="flex items-center">
+                  {{ project.name
+                  }}<span
+                    class="bg-blue-100 text-emerald-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-emerald-900 dark:text-emerald-300 ml-3"
+                    >Development with {{ project.development }}</span
+                  >
+                </h3>
+                <p
+                  class="mb-4 text-base font-normal text-white dark:text-gray-400"
+                >
+                  {{ project.description }}
+                </p>
+                <div class="flex items-center space-x-4">
                   <template
                     v-if="
                       project.link_github != '' &&
@@ -84,46 +96,57 @@ const store = useDataStore();
                     ><button
                       type="button"
                       @click="redirectTo(project.link_github)"
-                      class="flex justify-center items-center text-black transition ease-in-out delay-50 bg-white hover:-translate-y-1 hover:scale-110 focus:outline-none hover:bg-emerald-100 focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-emerald-500 dark:text-white dark:hover:bg-emerald-400"
+                      class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                     >
                       <font-awesome-icon
                         icon="fa-brands fa-github"
-                        class="w-6 h-6 fill-current text-black dark:text-white mr-2"
+                        class="w-4 h-4 fill-current text-black dark:text-white mr-2"
                         beat
-                        style="--fa-animation-duration: 2.5s"
+                        style="--fa-animation-duration: 5s"
                       />
                       GitHub
                     </button>
                   </template>
-                </div>
-                <template
-                  v-if="
-                    project.link_website != '' &&
-                    project.hasOwnProperty('link_website')
-                  "
-                >
-                  <button
-                    type="button"
-                    @click="redirectTo(project.link_website)"
-                    class="text-white transition ease-in-out delay-50 bg-emerald-400 hover:-translate-y-1 hover:scale-110 hover:bg-emerald-500 duration-300 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                  <template
+                    v-if="
+                      project.link_website != '' &&
+                      project.hasOwnProperty('link_website')
+                    "
                   >
-                    Look the project!
-                  </button>
-                </template>
-              </div>
-            </article>
-          </template>
+                    <button
+                      type="button"
+                      @click="redirectTo(project.link_website)"
+                      class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                    >
+                      Look the project
+                      <font-awesome-icon
+                        class="ml-2"
+                        icon="fa-solid fa-arrow-right"
+                      />
+                    </button>
+                  </template>
+                </div>
+              </li>
+            </template>
+          </ol>
         </div>
       </div>
     </section>
-    <section id="knowledge" class="bg-transparent dark:bg-transparent my-16">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path
+        fill="#10b981"
+        fill-opacity="1"
+        d="M0,192L30,197.3C60,203,120,213,180,181.3C240,149,300,75,360,69.3C420,64,480,128,540,181.3C600,235,660,277,720,272C780,267,840,213,900,176C960,139,1020,117,1080,122.7C1140,128,1200,160,1260,186.7C1320,213,1380,235,1410,245.3L1440,256L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
+      ></path>
+    </svg>
+    <section id="knowledge" class="bg-emerald-500 dark:bg-transparent py-16">
       <div class="py-16 px-4 mx-auto max-w-screen-xl lg:px-6">
         <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
           <h1
             class="my-4 text-4xl leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
           >
             <span
-              class="text-transparent font-medium bg-clip-text bg-gradient-to-r to-teal-700 from-emerald-700"
+              class="text-transparent font-medium bg-clip-text bg-black dark:bg-white"
               >Knowledge</span
             >
           </h1>
