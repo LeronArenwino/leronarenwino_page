99873b3a2b3b3086b0be540335613f5ca3fd4071
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-04-07 20:03:48 -0500

    :zap: [UPDATE] Modify state

--- src/stores/index.js
+++ src/stores/index.js
@@ -7,7 +7,7 @@ const app = initializeApp(config.firebaseConfig);
 const db = getFirestore(app);
 
 const projectsCollection = "projects";
-const knowledgeCollection = "knowledges";
+const skillsCollection = "knowledges";
 
 // You can name the return value of `defineStore()` anything you want, but it's best to use the name of the store and surround it with `use` and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
 // the first argument is a unique id of the store across your application
@@ -15,7 +15,7 @@ export const useDataStore = defineStore("data", {
   // other options...
   state: () => ({
     projects: [],
-    knowledges: [],
+    skills: [],
     products: [],
   }),
   getters: {},
@@ -29,12 +29,12 @@ export const useDataStore = defineStore("data", {
       });
       this.projects.sort((a, b) => b.date_published - a.date_published);
     },
-    async getKnowledges() {
-      this.knowledges = [];
-      const querySnapshot = await getDocs(collection(db, knowledgeCollection));
+    async getSkills() {
+      this.skills = [];
+      const querySnapshot = await getDocs(collection(db, skillsCollection));
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
-        this.knowledges.push({ ...doc.data(), id: doc.id });
+        this.skills.push({ ...doc.data(), id: doc.id });
       });
     },
     async getProducts(urlApi) {
