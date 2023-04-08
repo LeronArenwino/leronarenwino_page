cc0d11f506b3433c146e90bddbc23e5e2067e30f
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-04-07 20:14:26 -0500

    :zap: [UPDATE] Using skills colletion

--- src/stores/index.js
+++ src/stores/index.js
@@ -7,7 +7,7 @@ const app = initializeApp(config.firebaseConfig);
 const db = getFirestore(app);
 
 const projectsCollection = "projects";
-const skillsCollection = "knowledges";
+const skillsCollection = "skills";
 
 // You can name the return value of `defineStore()` anything you want, but it's best to use the name of the store and surround it with `use` and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
 // the first argument is a unique id of the store across your application
