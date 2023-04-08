d0bad3bbea1c0efd140636f3df447d8299f3d743
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-04-07 21:54:01 -0500

    :fire: [DELETE] Remove useless code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -83,7 +83,7 @@ const store = useDataStore();
                   >
                 </h3>
                 <span
-                  class="inline md:hidden bg-blue-100 text-emerald-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-emerald-900 dark:text-emerald-300 ml-3"
+                  class="inline md:hidden bg-blue-100 text-emerald-800 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-emerald-900 dark:text-emerald-300"
                   >{{ project.development }}</span
                 >
                 <p
