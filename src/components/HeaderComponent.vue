c51726a60a71b838769da4e325073e3b8016c430
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-04-09 14:32:58 -0500

    :zap: [UPDATE] New design to HeaderComponent

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -33,7 +33,7 @@
             <template v-if="$route.path == '/'">
               <li>
                 <a
-                  class="block py-2 pr-4 pl-3 text-white bg-emerald-700 rounded md:bg-transparent md:text-emerald-700 md:p-0 dark:text-white"
+                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-emerald-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   aria-current="page"
                   href="#app"
                   >Home</a
@@ -44,7 +44,7 @@
                   class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-emerald-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   aria-current="page"
                   href="#portfolio"
-                  >Portfolio</a
+                  >Projects</a
                 >
               </li>
               <li>
